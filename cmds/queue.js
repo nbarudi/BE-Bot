446fd14e071da52fbb5d94107c3cf34e30e7e36b
nbarudi <nbarudi@outlook.com>
2018-01-26 17:33:35 -0500

    Bug fixesssssssssss

--- cmds/queue.js
+++ cmds/queue.js
@@ -2,18 +2,19 @@ const Discord = module.require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-for (var songName in bot.queuenames){
   var mess = "```"
-  for (var i = 0; i < bot.queuenames.length; i++) {
+  for (var i = 0; i < bot.queuenames.length; i++ ) {
     var temp = (i + 1) + " : " + bot.queuenames[i] + (i === 0 ? "**(Current Songs)**" : "") + "\n"
     if ((mess + temp) <= 2000 - 3) {
       mess += temp
 
     }else {
+      mess += "```"
       message.channel.send(mess)
+      mess = "```"
     }
   }
-}
+mess += "```"
 message.channel.send(mess)
 
 }
