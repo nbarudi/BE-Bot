5f37d4aa7c6f2e916b2b3e65b0c87b71f2747c6f
nbarudi <nbarudi@outlook.com>
2018-01-26 17:22:37 -0500

    Bug fixes

--- cmds/queue.js
+++ cmds/queue.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
 for (var songName in bot.queuenames){
   var mess = "```"
-  for (var i = 0; i < bot.queuenames.length ) {
-    var temp = (i + 1) + " : " + queuenames[i] + (i === 0 ? "**(Current Songs)**" : "") + "\n"
+  for (var i = 0; i < bot.queuenames.length; i++) {
+    var temp = (i + 1) + " : " + bot.queuenames[i] + (i === 0 ? "**(Current Songs)**" : "") + "\n"
     if ((mess + temp) <= 2000 - 3) {
       mess += temp
 
